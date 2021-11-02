689f5219113e34cb6722b12a3f7bbc3d61455c49
thisisempty <thisisemptyyy@gmail.com>
2021-11-03 04:22:54 +0900

    [ADD] CORS

--- run.py
+++ run.py
@@ -2,13 +2,14 @@ from flask_mongoengine  import MongoEngine
 from flask_bcrypt       import Bcrypt
 from flask_restful      import Api
 from flask_jwt_extended import JWTManager
+from flask_cors         import CORS
 
 from routes  import initialize_routes
 from app     import get_app_with_config
 from config  import Runconfig
 
 app = get_app_with_config(Runconfig)
-
+CORS(app)
 db     = MongoEngine()
 api    = Api(app)
 bcrypt = Bcrypt(app)
@@ -19,4 +20,4 @@ db.init_app(app)
 initialize_routes(api)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
